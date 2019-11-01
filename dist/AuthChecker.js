54ff7cb73031d6b1d3898970400f89d2aaa0ba7c
Gregory Elliot <gregm@pairity.ai>
2019-11-01 18:32:27 -0400

    removed dist from git

--- dist/AuthChecker.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CustomAuthChecker = ({ root, args, context, info }, roles) => {
-    console.log(root, args, context.req.headers, info, roles);
-    return true;
-};
-exports.default = CustomAuthChecker;
-//# sourceMappingURL=AuthChecker.js.map
