e739bd7320f633a130e52a204b819fe2cab135ed
Gregory Elliot <gregm@pairity.ai>
2019-11-01 22:53:06 -0400

    removed log

--- src/CustomAuthChecker.ts
+++ src/CustomAuthChecker.ts
@@ -14,8 +14,6 @@ const CustomAuthChecker: AuthChecker<MyContextType> =
             [context.user.userId]
         );
 
-        console.log ('here');
-
         const assignedRoles = assignedRolesObjects.map((a: Role) => a.name);
 
         for(let i =0; i< roles.length; i++) {
